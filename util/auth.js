075485ffec2804eb4a82cb54eaba2c8a81406093
Hilmi GUNER <hilmiguner@Hilmi-MacBook-Air.local>
2023-07-26 15:42:03 +0300

    commit

--- util/auth.js
+++ util/auth.js
@@ -2,13 +2,25 @@ import axios from "axios"
 
 const API_KEY = "AIzaSyAMkbi5-uJ64QZKdSs1yYiKviOgYhJn7Xs";
 
-export async function createUser(email, password) {
+async function authenticate(mode, email, password) {
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
     const response = await axios.post(
-        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+        url,
         {
             email: email,
             password: password,
             returnSecureToken: true,
         }
     );
+
+    const token = response.data.idToken;
+    return token;
+}
+
+export function createUser(email, password) {
+    return authenticate("signUp", email, password);
+}
+
+export function login(email, password) {
+    return authenticate("signInWithPassword", email, password);
 }
