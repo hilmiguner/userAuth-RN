fd21e093cddc59487557b642c10d4aeacc74ad5d
Hilmi GUNER <hilmiguner@Hilmi-MacBook-Air.local>
2023-07-26 16:05:24 +0300

    commit

--- App.js
+++ App.js
@@ -7,7 +7,8 @@ import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import { Colors } from './constants/styles';
-import AuthContextProvider from './store/auth-context';
+import AuthContextProvider, { AuthContext } from './store/auth-context';
+import { useContext } from 'react';
 
 const Stack = createNativeStackNavigator();
 
@@ -41,12 +42,13 @@ function AuthenticatedStack() {
 }
 
 function Navigation() {
+  const authCtx = useContext(AuthContext);
+
   return (
-    <AuthContextProvider>
       <NavigationContainer>
-        <AuthStack />
+        {!authCtx.isAuthenticated && <AuthStack />}
+        {authCtx.isAuthenticated && <AuthenticatedStack />}
       </NavigationContainer>
-    </AuthContextProvider>
   );
 }
 
@@ -54,8 +56,9 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-
-      <Navigation />
+      <AuthContextProvider>
+        <Navigation />
+      </AuthContextProvider>
     </>
   );
 }
