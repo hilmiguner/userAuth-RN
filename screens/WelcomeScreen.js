5b0187bd7f13a18e8c5e809e6dcb7f2b666526c8
Hilmi GUNER <hilmiguner@Hilmi-MacBook-Air.local>
2023-07-26 17:16:41 +0300

    commit

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native';
 
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../store/auth-context';
+
 function WelcomeScreen() {
+  const [fetchedMessage, setFecthedMessage] = useState("");
+
+  const authCtx = useContext(AuthContext);
+
+  const authToken = authCtx.token;
+
+  useEffect(() => {
+    axios.get(`https://react-native-firebase-6caa6-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=${authToken}`).then((response) => {
+      setFecthedMessage(response.data);
+    })
+  }, [authToken]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
